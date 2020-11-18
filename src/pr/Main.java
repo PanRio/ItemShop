157c7e9f84fe27d3829d2fd826ecb83297e70946
Roberto <roberto.laczak@gmail.com>
2020-11-18 20:30:24 +0100

    test2

--- src/pr/Main.java
+++ src/pr/Main.java
@@ -1,7 +1,29 @@
 package pr;
 
-public class Main{
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.plugin.PluginBase;
+
+public class Main extends PluginBase {
+
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+
+
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		return super.onCommand(sender, command, label, args);
+	}
+
 	
-	
-	int test; //Mateusz ma długiego
+
+	int test;
 }
