2e83ac1e7391a4426f0deddc1ff5f4048dbb9dee
Fred <freedommo33@gmail.com>
2020-11-19 22:38:42 +0100

    npc wand

--- src/pr/ShopGui.java
+++ src/pr/ShopGui.java
@@ -5,7 +5,7 @@ import cn.nukkit.block.BlockID;
 import cn.nukkit.inventory.Inventory;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemSteak;
-import com.sun.prism.Material;
+//import com.sun.prism.Material;
 
 public class ShopGui {
 
